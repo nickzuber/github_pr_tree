52be391cc80c0bbc55b3e74b3c1ea7db34fd0db8
Nick Zuber <nicholas@robinpowered.com>
2019-09-16 16:31:39 -0400

    Make it a little prettier

--- src/js/components/file/index.jsx
+++ src/js/components/file/index.jsx
@@ -49,9 +49,13 @@ class File extends React.Component {
     const index = filter ? (name.toLowerCase() || '').indexOf(filter.toLowerCase()) : -1
     const highlightedName = (index === -1) ? name : this.getHighlight({ name, filter, index })
 
+    console.warn(name, options);
+
     return (
       <div className={topClassName}>
-        <span className={`icon ${className}`} />
+        <span className="icon">
+          <svg aria-label="file" class="octicon octicon-file" viewBox="0 0 12 16" version="1.1" width="12" height="16" role="img"><path fill-rule="evenodd" d="M6 5H2V4h4v1zM2 8h7V7H2v1zm0 2h7V9H2v1zm0 2h7v-1H2v1zm10-7.5V14c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1V2c0-.55.45-1 1-1h7.5L12 4.5zM11 5L8 2H1v12h10V5z"></path></svg>
+        </span>
         <a href={href} className='link-gray-dark'>{highlightedName}</a>
         {options.diffStats && diffStats && <DiffStats diffStats={diffStats} />}
         {hasComments ? (
